8d941c36f22f2633990a0bf253e877dc5b6ef0f6
Hajir Rasouli <54510090+hazhir95@users.noreply.github.com>
2023-10-21 01:57:09 -0400

    Update script.js
    Fixed interfering audios

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function playAudio(audioId) {
+    // Pause all audio elements
+    document.getElementById('Document1').pause();
+    document.getElementById('Document2').pause();
+    document.getElementById('Document3').pause();
+
+    
+    // Play the selected audio
     const audioElement = document.getElementById(audioId);
-    audioElement.play();
+    if (audioElement) {
+        audioElement.play();
+    }
+    
 }
